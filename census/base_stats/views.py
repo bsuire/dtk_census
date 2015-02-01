637b3b9416d39016281f162d6d9953c742fa33c8
Benjamin Suire <bensuire@gmail.com>
2015-02-01 18:04:18 +0100

    Computes count and average age for each field/variable

--- census/base_stats/views.py
+++ census/base_stats/views.py
@@ -13,8 +13,60 @@ def stats(request):
     #   
     # TODO : keep valeus in memory? (cache)
 
-    c  = CensusLearnSql.objects.values('class_of_worker').order_by('age').distinct()
+    # get field names  
+    fields = CensusLearnSql._meta.get_all_field_names()
+    fields.remove('age')
     
+    # get the distinct values for each field
+    for field in fields:
+        f = field
+
+        values = CensusLearnSql.objects.values(field).order_by().distinct()
+        
+        # get count and average age for each valu
+        for value in values:
+            v = value
+
+            if value[field] is not None:
+
+                kwargs = {
+                    '{0}'.format(field):  value[field] 
+                }
+                
+                ages = CensusLearnSql.objects.values('age').filter(**kwargs)
+                
+                # takes twice as long if obtaining count from query
+                #count = ages.count()
+                
+                count = 0
+                average = 0
+                for age in ages:
+                    count += 1
+                    average += age['age']
+
+                
+                average /= count
+                
+                print field
+                print value[field]
+                print count
+                print average
+
+                
+            # used to make sure that all ages field values are None when country_of_birth is None
+                #ages=dict(ages[0])
+                #for key,value in ages.items():
+                #    print key,value
+                #    if value is None:
+                #          del ages[key]
+                #          print "popped"
+
+        
+        break
+
+            
+    c = 'hello'
+
     data=str(c)
     
     context = {'data': data} 
