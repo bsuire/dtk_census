bed881a9369119bafb945650364e17d7656427d8
Benjamin Suire <bensuire@gmail.com>
2015-02-02 17:19:09 +0100

    Fetching data per variable in order

--- census/base_stats/views.py
+++ census/base_stats/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from models import CensusLearnSql
 # Create your views here.
 
+DEBUG = False
+MAX = 8
+
 def stats(request):
     
     # read from DB, compute all counts/averages, and organize
@@ -18,47 +21,72 @@ def stats(request):
     # get field names  
     fields = CensusLearnSql._meta.get_all_field_names()
     fields.remove('age')
-    
+    fields.remove('id') 
+
     # get the distinct values for each field
-    for field in fields:
-
-        variables = dict()
-
-        values = CensusLearnSql.objects.values('age',field).order_by()
-        #print values
-       
-        for value in values:
-            #print value
-
-            if value['age'] is None:
-                continue
-            
-            # variable is already in dictionary
-            if value[field] in variables:
-                
-                (average,count) = variables[value[field]]
-                average += value['age']
-                count += 1
-                
-            # new entry
-            else:
-                average = value['age']
-                count = 1
-            
-            variables[value[field]] = average,count
-
-        
-        # aggregation complete, now compute averages
-        for key, value in variables.iteritems():
-            
-            variables[key] = value[0]/value[1],value[1]
-            print key,variables[key]
-       
-            
-    c = variables
-
-    data=str(c)
+    for x,field in enumerate(fields):
     
-    context = {'data': data} 
+        if x == MAX and DEBUG: break   
+     
+        print str(x+1) + '/' + str(len(fields)) + '\t' + field 
+        
+        # stats = { (value,age,count), ... }
+        stats = get_base_stats(field)
+        
+        # sort stats from high to low count
+        stats.sort(key=lambda tup: tup[2], reverse=True)
+        
+        context[field] = stats 
+    
     return render(request,'stats.html',context)
+
+
+
+def get_base_stats(field): 
+
+    variables = dict()
+
+    values = CensusLearnSql.objects.values('age',field)
+   
+    for value in values:
+
+        if value['age'] is None:
+            continue
+        
+        # variable is already in dictionary
+        if value[field] in variables:
+            
+            (average,count) = variables[value[field]]
+            average += value['age']
+            count += 1
+            
+        # new entry
+        else:
+            average = value['age']
+            count = 1
+        
+        variables[value[field]] = average,count
+    
+    # aggregation complete, now compute averages
+    
+    var_list = []
+    
+    for key, value in variables.iteritems():
+        
+        var_list.append((key, value[0]/value[1], value[1]))
+
+        #variables[key] = value[0]/value[1],value[1]
+        #print key,variables[key]
+    
+    return var_list 
+
+def sort_stats(field,stats):
+    # stats = {(value,age,count_max), ... , (value,age,count_min)}
+    #data.sort(key=lambda tup: tup[2])  # sorts in place 
+    pass
+
+
+
+
+
 
