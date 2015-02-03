ba078959256709db1ed69f4664b9d777f1cf0238
Benjamin Suire <bensuire@gmail.com>
2015-02-03 23:39:55 +0100

    Settup backend so that data for each variable is only fetched when needed

--- census/base_stats/urls.py
+++ census/base_stats/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from base_stats import views
 
 urlpatterns = [
-        url(r'^$', views.stats, name='stats'),
+        url(r'^$', views.home, name='home'),
+        url(r'^data/(?P<variable>\w+)/$', views.stats, name='stats'),
     ]
--- census/base_stats/views.py
+++ census/base_stats/views.py
@@ -1,45 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from models import CensusLearnSql
-# Create your views here.
+import json
 
-DEBUG = False
-MAX = 8
 
-def stats(request):
-    
-    # read from DB, compute all counts/averages, and organize
-
-    # variable 
-    #         \____ val 1 = { count, avg} 
-    #          \___ val 2
-    #           \__ val 3
-    #   
-    # TODO : keep values in memory? (cache)
+def home(request):
 
     context = {} 
     
-    # get field names  
-    fields = CensusLearnSql._meta.get_all_field_names()
-    fields.remove('age')
-    fields.remove('id') 
-
-    # get the distinct values for each field
-    for x,field in enumerate(fields):
-    
-        if x == MAX and DEBUG: break   
-     
-        print str(x+1) + '/' + str(len(fields)) + '\t' + field 
-        
-        # stats = { (value,age,count), ... }
-        stats = get_base_stats(field)
-        
-        # sort stats from high to low count
-        stats.sort(key=lambda tup: tup[2], reverse=True)
-        
-        context[field] = stats 
-    
     return render(request,'stats.html',context)
 
+
+
+def stats(request,variable):
+    
+    print "User requested stats on " + variable
+
+    # stats = { (value,age,count), ... }
+    stats = get_base_stats(variable)
+    
+    # sort stats from high to low count
+    #TODO return as dict, and use order by to sort in browser?
+    #stats.sort(key=lambda tup: tup[2], reverse=True)
+    
+    # return data as JSON, without rendering
+    return HttpResponse(json.dumps(stats), content_type="application/json")
 
 
 def get_base_stats(field): 
@@ -72,21 +57,36 @@ def get_base_stats(field):
     var_list = []
     
     for key, value in variables.iteritems():
-        
-        var_list.append((key, value[0]/value[1], value[1]))
-
+       
         #variables[key] = value[0]/value[1],value[1]
-        #print key,variables[key]
+        #var_list.append(key, value[0]/value[1], value[1]))
+
+        entry = {} 
+        entry["name"] = key
+        entry["count"] = value[1]
+        entry["age"] = value[0]/value[1]
+        
+        var_list.append(entry) 
     
-    return var_list 
+    return var_list
 
-def sort_stats(field,stats):
-    # stats = {(value,age,count_max), ... , (value,age,count_min)}
-    #data.sort(key=lambda tup: tup[2])  # sorts in place 
-    pass
-
-
-
-
-
-
+## get all fields....
+#    # get field names  
+#    fields = CensusLearnSql._meta.get_all_field_names()
+#    fields.remove('age')
+#    fields.remove('id') 
+#
+#    # get the distinct values for each field
+#    for x,field in enumerate(fields):
+#    
+#        if x == MAX and DEBUG: break   
+#     
+#        print str(x+1) + '/' + str(len(fields)) + '\t' + field 
+#        
+#        # stats = { (value,age,count), ... }
+#        stats = get_base_stats(field)
+#        
+#        # sort stats from high to low count
+#        stats.sort(key=lambda tup: tup[2], reverse=True)
+#        
+#        context[field] = stats 
