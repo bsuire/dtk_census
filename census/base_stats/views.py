5e2379610a3a2c06d1eab30970db55f4e76be535
Benjamin Suire <bensuire@gmail.com>
2015-02-05 03:03:35 +0100

    Added computation and display of the number of values and rows clipped out

--- census/base_stats/views.py
+++ census/base_stats/views.py
@@ -51,7 +51,9 @@ def stats(request,field): # note: fields and variables are used interchangeably
     values_aggreg = dict()
     
     values = CensusLearnSql.objects.values('age',field)
-   
+
+    nb_rows_valid = 0
+
     # iterate through query results to aggregate count and age
     for value in values:
         
@@ -60,6 +62,8 @@ def stats(request,field): # note: fields and variables are used interchangeably
         # but I have not verified this assumption)
         if value['age'] is None:
             continue
+        
+        nb_rows_valid += 1
         
         # variable is already in dictionary. Update...
         if value[field] in values_aggreg:
@@ -90,6 +94,58 @@ def stats(request,field): # note: fields and variables are used interchangeably
         
         var_list.append(entry) 
     
+    # sort entries by decreasing count 
+    var_list.sort(key=lambda k: k["count"], reverse=True) 
+    
+    # get number of values clipped out (doens't include values of type None, which were filtered out earlier)
+    nb_values_hidden = clip(var_list)
+    
+    # get number of rows clipped out (excluding rows of None type)
+    nb_rows_shown = aggregate_count(var_list)
+    nb_rows_hidden = nb_rows_valid - nb_rows_shown
+    
+    meta = {}
+    meta["values_clipped"] = nb_values_hidden
+    meta["rows_clipped"] = nb_rows_hidden
+
+    print "Nb of values that were clipped out: " + str(nb_values_hidden)
+    print "Nb of rows that were clipped out: " + str(nb_rows_hidden)
+    print "Total number of VALID rows: " + str(nb_rows_valid)
+    print "Sanity check: nb_shown + nb_hidden = ? " + str(nb_rows_shown + nb_rows_hidden)
+    
+    response_data = {} 
+    response_data["meta"] = meta
+    response_data["data"] = var_list
+
     # return data to client as JSON 
-    return HttpResponse(json.dumps(var_list), content_type="application/json")
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+
+def clip(list_rows):
+    
+    nb_rows = len(list_rows)
+    
+    if nb_rows <= 100:
+        return 0
+
+    else:
+        del list_rows[100:]
+        return nb_rows - 100
+
+
+def aggregate_count(list_rows):
+    
+    count_total = 0
+    
+    for row in list_rows:
+        count_total += row["count"]
+
+    return count_total
+
+
+
+
+
+
+
 
